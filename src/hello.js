f0100b374f0a2e276f6812e5ca692e0bb4996451
Anchit Aggarwal <M1042942@mindtree.com>
2019-04-09 13:23:07 +0530

    test library

---
+++ src/hello.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import ReactDom from 'react-dom';
+
+class Hello extends Component{
+
+    render(){
+        return(
+            <div>Hello World</div>
+        );
+    }
+
+}
+
+export default Hello;
